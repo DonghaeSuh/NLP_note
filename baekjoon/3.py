ba66708b175f1feabc204365b797eb3a5cb083c4
donghaesuh <donghaeseo2@gmail.com>
2022-02-12 23:48:43 +0900

    20220212

---
+++ baekjoon/3.py
@@ -0,0 +1,34 @@
+# 2739
+"""
+a=int(input()) 
+for i in range(1,10):
+    print("{} * {} = {}".format(a,i,i*a))  # print(f'{a} * {i} = {a*i}')
+"""
+# 10950
+"""
+l = range(int(input()))
+result =[]
+for i in l:
+    a,b=map(int,input().split())
+    result.append(a+b)
+for w in l:
+    print(result[w])
+"""
+
+"""
+입력을 다 받고 한꺼번에 출력해야 하는 줄 알앗으나 아니였음
+t = int(input())
+
+for result in range(t):
+    A, B = map(int, input().split())
+    print(A+B)
+
+"""
+# 8393
+"""
+a= int(input())
+b=0
+for i in range(a):
+    b+=i+1
+print(b)
+"""
