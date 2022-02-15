bfe8378440a10ec65b0f9ed1e6e0d643c734d27d
donghaesuh <donghaeseo2@gmail.com>
2022-02-15 22:54:15 +0900

    20220215

---
+++ baekjoon/5.py
@@ -0,0 +1,12 @@
+#10818
+"""
+a=int(input())
+b=list(map(int,input().split()))
+print(f"{min(b)} {max(b)}")
+"""
+"""
+n=int(input())
+L=list(map(int, input().split()))
+L.sort()
+print(L[0],L[-1])
+"""
