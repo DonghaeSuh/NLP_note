2940af138345ef1f037cc83704004f5a30928309
DonghaeSuh <gypsi12@naver.com>
2022-03-03 22:23:46 +0900

    20220303

--- baekjoon/8.py
+++ baekjoon/8.py
@@ -69,6 +69,21 @@ while (a*d-b*(d-1))<c:
     d+=1
 print(d)
 """
+"""
 a,b,v = map(int,input().split())
 k = (v-b)/(a-b)
 print(int(k) if k == int(k) else int(k)+1)
+"""
+
+
+#10250
+t = int(input())
+
+for i in range(t):
+    h, w, n = map(int, input().split())
+    num = n//h + 1
+    floor = n % h
+    if n % h == 0:  # h의 배수이면,
+        num = n//h
+        floor = h
+    print(f'{floor*100+num}')
